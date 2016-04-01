bbc0faff143a1bcd892de6a25a8569b73137b706
Mauro Franceschini <mauro.franceschini@gmail.com>
2016-04-01 19:59:02 +0200

    Added core module configuration

---
+++ src/app/core/core.module.js
@@ -0,0 +1,28 @@
+/**
+ * Created by mauro on 01/04/16.
+ */
+(function () {
+  'use strict';
+
+  angular
+    .module('app.core', [
+      /*
+       * Angular modules
+       */
+      'ngAnimate',
+      'ngSanitize',
+      'ngResource',
+      /*
+       * Our reusable cross app code modules
+       */
+      'blocks.exception',
+      'blocks.logger',
+      'blocks.router',
+      /*
+       * 3rd Party modules
+       */
+      'ui.bootstrap',
+      'pascalprecht.translate'
+    ]);
+
+})();
